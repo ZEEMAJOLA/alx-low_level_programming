93cce4d3d7e19e765f36db5a21a9ceb2b51523e4
ZEEMAJOLA <majolazinhle22@gmail.com>
2023-04-20 09:17:41 -0700

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - A program that prints a line using printf function
- * return: Always 0(success)
+ * Return: Always 0(success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
  * Return: Always 0 (success)

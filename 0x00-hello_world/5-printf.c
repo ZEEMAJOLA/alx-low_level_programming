0741e6f223a3380407f4e1ab2ae2931620bea55c
ZEEMAJOLA <majolazinhle22@gmail.com>
2023-04-20 09:09:15 -0700

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - A program that prints a line using printf function
- * return: 0
+ * return: Always 0(success)
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }

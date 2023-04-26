a1419d8fb347acb1c5dda104d6ace7db1468cabb
ZEEMAJOLA <majolazinhle22@gmail.com>
2023-04-26 15:04:10 -0700

    _putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,26 @@
 #include "main.h"
+
 /**
- * print_sign - shows if number is greater, equal
- * or less than zero
+ * print_sign - print the sign of a number
+ * @n: The input number which will be printed by a sign.
  *
- * @n: The input nuber is an integer.
- *
- * Return: 1 if number if greater than zero. 0 if number is equal to zero.
+ * Return: 1 if number is greater than zero. 0 if number is equal to zero.
  * -1 if number is less than zero.
  */
 {
 	if (n > 0)
 	{
-		_putchar(43);
-		return(1);
+		_putchar('+');
+		return (1);
 	}
 	else if (n < 0)
-	{ 
-		_putchar(45);
-		return(-1);
+	{
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	}
-	_putchar('\n');
 }

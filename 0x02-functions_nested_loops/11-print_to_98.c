f6d8f8abe2449c67141a445bed79cb0666a9884e
ZEEMAJOLA <majolazinhle22@gmail.com>
2023-04-26 15:55:54 -0700

    _putchar

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,17 +7,11 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
+	if (n <= 98)
 	{
-		while (n > 98)
-			_putchar("%d, ", n--);
-		_putchar("%d\n", n);
-	}
-
-	else
-	{
-		while (n < 98)
-			_putchar("%d, ", n++);
-		_putchar("%d\n", n);
+		_putchar(n);
+		continue; (n++)
+		_putchar(',');
+		_putchar(' ');
 	}
 }
